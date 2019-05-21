1164e475bcd3b9ff9c47284537b70d38c2fa9641
Admin <Admin@DESKTOP-89NTJEN>
2019-05-22 10:52:58 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace specifiedoperations1
+{
+    class Program
+    {
+        // Input a test data and getting the expected value
+        static void Main(string[] args)
+        {
+            Console.Write("The Result is :");
+            Console.WriteLine(-1 + 4 * 6 );
+            Console.Write("The Result is :");
+            Console.WriteLine((35 + 5) % 7);
+            Console.Write("The Result is :");
+            Console.WriteLine(14 + -4 * 6 / 11);
+            Console.Write("The Result is :");
+            Console.WriteLine(2 + 15 / 6 * 1 - 7 % 2);
+        }
+    }
+}
